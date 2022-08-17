65d5f0453a649c5e365af67b7e326e863afe2039
Ivan Pizarroso <ivnp31@gmail.com>
2022-08-17 12:01:38 +0200

    final

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+const buttonMenuOpen = document.querySelector(".header__menu");
+const buttonMenuClose = document.querySelector(".mobile__menu__close");
+const mobileMenu = document.querySelector(".mobile__menu");
+const button = document.querySelector(".mobile__menu__button");
+let menu = false;
 
+buttonMenuOpen.addEventListener("click", () => {
+  showMenu();
+});
+
+buttonMenuClose.addEventListener("click", () => {
+  showMenu();
+});
+
+button.addEventListener("click", () => {
+  showMenu();
+});
+
+const showMenu = () => {
+  menu = !menu;
+
+  if (menu) {
+    mobileMenu.classList.add("show");
+  } else {
+    mobileMenu.classList.remove("show");
+  }
+};
